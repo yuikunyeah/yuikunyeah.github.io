dba145a11a538101df5f360276e3ad3bebb4d28e
yuikunyeah <72680795+yuikunyeah@users.noreply.github.com>
2021-05-29 22:49:24 +0900

    Update min.js

--- min.js
+++ min.js
@@ -1,1 +1,19 @@
 
+$(finction(){
+  $(".btn-gnavi").on("click", function(){
+//メニューの位置
+  var rightVal = 0;
+  if($(this).hasClass("open")){//開いているなら
+  //閉じる
+    rightval = -30;
+    //クラスを消す
+    $(this).removeClass("open");
+    ]else{//閉じている
+    //クラス生成
+    $(this).addClass("open");
+    }
+$("#grobal-navi").stop().animate({
+  right: rightVal
+},200);
+  });
+});
