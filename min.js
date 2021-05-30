50d0dbeeb27ee21764f7de7fd8200b6b91ea77b8
yuikunyeah <72680795+yuikunyeah@users.noreply.github.com>
2021-05-30 15:01:37 +0900

    Update min.js

--- min.js
+++ min.js
@@ -1,19 +1,20 @@
-
 $(function(){
-  $(".btn-gnavi").on("click", function(){
-//メニューの位置
-  var rightVal = 0;
-  if($(this).hasClass("open")){//開いているなら
-  //閉じる
-    rightval = -30;
-    //クラスを消す
-    $(this).removeClass("open");
-  }else{//閉じている
-    //クラス生成
-    $(this).addClass("open");
-    }
-$("#grobal-navi").stop().animate({
-  right: rightVal
-},200);
-  });
+	$(".btn-gnavi").on("click", function(){
+		// ハンバーガーメニューの位置を設定するための変数
+		var rightVal = 0;
+		if($(this).hasClass("open")) {
+			// 「open」クラスを持つ要素はメニューを開いた状態に設定
+			rightVal = -300;
+			// メニューを開いたら次回クリック時は閉じた状態になるよう設定
+			$(this).removeClass("open");
+		} else {
+			// 「open」クラスを持たない要素はメニューを閉じた状態に設定 (rightVal は0の状態 )
+			// メニューを開いたら次回クリック時は閉じた状態になるよう設定
+			$(this).addClass("open");
+		}
+
+		$("#global-navi").stop().animate({
+			right: rightVal
+		}, 200);
+	});
 });
