f75bc2d312b4c2ac5fd7d4e9d18bde35953aba39
yuikunyeah <72680795+yuikunyeah@users.noreply.github.com>
2021-05-30 21:41:06 +0900

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
-
 function Inbox(){	
-$(".btn-gnavi").on("click", function(){
+$(function(){
+	$(".btn-gnavi").on("click", function(){
 		// ハンバーガーメニューの位置を設定するための変数
 		var rightVal = 0;
 		if($(this).hasClass("open")) {
