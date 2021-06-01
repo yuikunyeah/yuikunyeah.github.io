27c26fab644dcbfdec9262b27297037a74f7a064
yuikunyeah <72680795+yuikunyeah@users.noreply.github.com>
2021-06-02 05:51:22 +0900

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,9 @@
+function gate() {
+var time =  new Date();  
+var watch = watch++;
+var people = document.getElementById("watch");
+people.innerHTML = <span>"あなたは+watch+番目のお客様です。"</span>
+var watchTime  = getElementById("time");
+watchTime.innerHTML = <span>getTime</span>
+}
+gate();
