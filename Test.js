bb99f0d29d69b5b1aecbafc9191deff87ba7022d
yuikunyeah <72680795+yuikunyeah@users.noreply.github.com>
2021-06-02 06:02:51 +0900

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,7 +2,7 @@ function gate() {
 var time =  new Date();  
 var watch = watch++;
 var people = document.getElementById("watch");
-people.innerHTML = <span>"あなたは+watch+番目のお客様です。"</span>
+people.innerHTML = "あなたは+watch+番目のお客様です。"
 var watchTime  = getElementById("time");
 watchTime.innerHTML = time;
 }
